1659b239ccbd99778eb74db7a998392cd971c4c5
Pedro Henriques Dias <diaspedro1999@hotmail.com>
2018-05-18 17:25:27 +0100

    commit 4
    Sign Up / Sign In pages

--- src/components/User/Signin.vue
+++ src/components/User/Signin.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <p>The Signin Page</p>
+    <div class="text-md-center">
+        <h2>Sign In</h2>
+        <v-layout>
+            <v-flex xs6 offset-xs3>
+                <v-card>
+                    <v-card-text>
+                        <v-text-field v-model="register.email" label="Email" required></v-text-field>
+                        <v-text-field v-model="register.password" label="Password" required></v-text-field>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                register:{
+                    email: "",
+                    password: ""
+                }
+            }
+        }
+    }
+</script>
--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -1,5 +1,30 @@
 <template>
-    <div>
-        <p>The Signup Page</p>
+    <div class="text-md-center">
+        <h2>Sign Up</h2>
+        <v-layout>
+            <v-flex xs6 offset-xs3>
+                <v-card>
+                    <v-card-text>
+                        <v-text-field v-model="register.username" label="Username" required></v-text-field>
+                        <v-text-field v-model="register.email" label="Email" required></v-text-field>
+                        <v-text-field v-model="register.password" label="Password" required></v-text-field>
+                    </v-card-text>
+                </v-card>
+            </v-flex>
+        </v-layout>
     </div>
 </template>
+
+<script>
+    export default {
+        data() {
+            return {
+                register:{
+                    username: "",
+                    email: "",
+                    password: ""
+                }
+            }
+        }
+    }
+</script>
